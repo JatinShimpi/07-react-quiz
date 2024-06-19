3d841e8f54d27b816aca0ca4a08577f76e3c59fb
JatinShimpi <shimpijatin184@gmail.com>
2024-06-19 23:27:09 +0530

    added new components

---
+++ src/components/Startscreen.js
@@ -0,0 +1,11 @@
+function Startscreen({numQuestions,dispatch}) {
+    return (
+        <div className="start">
+            <h2>Welcome to the react quiz</h2>
+            <h3>{numQuestions} question to test your react mastery</h3>
+            <button className="btn btn-ui" onClick={()=>dispatch({type:'start'})}>Let's start</button>
+        </div>
+    )
+}
+
+export default Startscreen
